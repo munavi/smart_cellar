d2f6c093b43e878fab8e2c81bac804ac5b0b319e
Natalya Murashko <natalya.murashko@hs-augsburg.de>
2023-05-08 19:34:14 +0200

    style: add stylelint, i18n plugin for eslint

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,29 +1,27 @@
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from './LangSwitcher.module.scss';
-import {useTranslation} from "react-i18next";
-import React from "react";
-import {Button, ThemeButton} from "shared/ui/Button/Button";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import React from 'react';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 interface LangSwitcherProps{
     className?: string,
 
 }
 
-export const LangSwitcher = ({className}: LangSwitcherProps) => {
-
-    const {t, i18n} = useTranslation();
+export const LangSwitcher = ({ className }: LangSwitcherProps) => {
+    const { t, i18n } = useTranslation();
 
     const toggle = () => {
-        i18n.changeLanguage(i18n.language === "de"? "en" : "de")
-    }
+        i18n.changeLanguage(i18n.language === 'de' ? 'en' : 'de');
+    };
 
     return (
-            <Button
-                className={classNames(cls.LangSwitcher, {}, [className])}
-                theme={ThemeButton.CLEAR}
-                onClick={toggle}
-            >
-                {t("Language")}
-            </Button>
+        <Button
+            className={classNames('', {}, [className])}
+            theme={ThemeButton.CLEAR}
+            onClick={toggle}
+        >
+            {t('Language')}
+        </Button>
     );
 };
