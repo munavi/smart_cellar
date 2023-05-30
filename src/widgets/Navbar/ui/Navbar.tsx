e8233d3b13c175213da47687004fc1ab1bb702f2
Natalya Murashko <natalya.murashko@hs-augsburg.de>
2023-05-30 17:48:16 +0200

    style:change sidebar layout, add some buttons, new test for sidebar, reg-cli for ui testing

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import React from 'react';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
@@ -14,18 +13,7 @@ export const Navbar = ({ className }: NavbarProps) => {
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink
-                    theme={AppLinkTheme.SECONDARY}
-                    to="/"
-                    className={cls.mainLink}
-                >
-                    {t('Main Page')}
-                </AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-                    {t('About Page')}
-                </AppLink>
-            </div>
+            <div className={cls.links} />
         </div>
     );
 };
