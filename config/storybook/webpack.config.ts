9cee3e3bedf3d76437ab6b93e7f92781712cd0ed
Natalya Murashko <natalya.murashko@hs-augsburg.de>
2023-06-23 22:49:23 +0200

    fix: change webpack.config for storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,4 +1,4 @@
-import webpack, { DefinePlugin, RuleSetRule } from 'webpack';
+import webpack, { DefinePlugin } from 'webpack';
 import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
@@ -10,26 +10,33 @@ export default ({ config }: {config: webpack.Configuration}) => {
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
-    config!.resolve!.modules!.push(paths.src);
+    config.resolve?.modules?.push(paths.src);
     config!.resolve!.extensions!.push('.ts', '.tsx');
 
-    // eslint-disable-next-line no-param-reassign
-    // @ts-ignore
-    config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
-        if (/svg/.test(rule.test as string)) {
-            return { ...rule, exclude: /\.svg$/i };
-        }
+    if (config!.resolve!.modules) {
+        config!.resolve!.modules = [
+            path.resolve(__dirname, '../../src'),
+            'node_modules',
+        ];
+    }
 
-        return rule;
-    });
+    if (config.module?.rules) {
+        config.module.rules = config.module?.rules?.map((rule: webpack.RuleSetRule| '...') => {
+            if (rule !== '...' && /svg/.test(rule.test as string)) {
+                return { ...rule, exclude: /\.svg$/i };
+            }
 
-    config!.module!.rules.push({
+            return rule;
+        });
+    }
+
+    config.module?.rules?.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
-    config!.module!.rules.push(buildCssLoader(true));
+    config.module?.rules?.push(buildCssLoader(true));
 
-    config!.plugins!.push(new DefinePlugin({
+    config.plugins?.push(new DefinePlugin({
         __IS_DEV__: JSON.stringify(true),
         __API__: JSON.stringify(''),
     }));
